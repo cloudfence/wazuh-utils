d7d8fa99acc930cefc1036617cf0431ce26fec0b
Julio Cesar Camargo (JCC) <julio@cloudfence.com.br>
2024-10-09 11:53:04 +0100

    Update wazuh-agent-linux-install.sh
    updated headers

--- wazuh-agent-linux-install.sh
+++ wazuh-agent-linux-install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-
-# Program to install Wazuh manager along Open Distro for Elasticsearch
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Copyright (C) 2022 Cloudfence Ltda
+#    Copyright (C) 2022 Cloudfence
+#    Copyright (C) 2015-2024 Wazuh Inc
+#    All rights reserved.
 #
-# This program is a free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public
-# License (version 2) as published by the FSF - Free Software
-# Foundation.
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.#
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+#    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+#    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#    POSSIBILITY OF SUCH DAMAGE.
 
 #prompt user for the required info
 echo -n "Wazuh Manager Address: "
